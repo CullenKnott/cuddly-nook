bf12b47174fe19fd16f47c5d00ecbd8cb8e90f35
Cullen Knott <cullenp.knott@gmail.com>
2023-05-22 09:42:14 -0700

    added local storage and dynamically changing HTML based on the time of day

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,48 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
+
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+
+  function currentTime() {
+   var time = dayjs().format('H') // current hour of the day
+   var timeBlock = $('.time-block')
+    console.log(time)
+
+    timeBlock.each(function () { // compares time to the id of the HTML and applies appropriate class
+      var idSelector = $(this).attr('id')
+      if (idSelector === time) {
+        $(this).attr('class', 'row time-block present')
+      } else if (idSelector > time) {
+        $(this).attr('class', 'row time-block past')
+      } else {
+        $(this).attr('class', 'row time-block future')
+      }
+    })
+  
+  }
+  currentTime() // calls function
+
+  // displays current day
+  var today = dayjs();
+  $('#currentDay').text(today.format('dddd, MMMM D')) // ordinals arent working :(
+  // saves to local storage
+  $('button').on('click', function(event) {
+    event.preventDefault();
+    var descrIndex = $(this).siblings('.description').val(); //traverse DOM to grab value in the textarea
+    var parentAttr = $(this).parent().attr('id')
+    localStorage.setItem(parentAttr, JSON.stringify(descrIndex)) // saves in separate storage containers
+  })
+
+  // gets local storage and applies to page 
+  $('#9 textarea').val(localStorage.getItem('9'))
+  $('#10 textarea').val(localStorage.getItem('10'))
+  $('#11 textarea').val(localStorage.getItem('11'))
+  $('#12 textarea').val(localStorage.getItem('12'))
+  $('#13 textarea').val(localStorage.getItem('13'))
+  $('#14 textarea').val(localStorage.getItem('14'))
+  $('#15 textarea').val(localStorage.getItem('15'))
+  $('#16 textarea').val(localStorage.getItem('16'))
+  $('#17 textarea').val(localStorage.getItem('17'))
 });
